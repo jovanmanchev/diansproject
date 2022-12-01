04bb946938171a679507fd3bc888e508e7b6befd
jovanmanchev <jovanmanchev3003@gmail.com>
2022-12-01 12:54:41 +0100

    spring initialized

---
+++ Domasna 2/tech prototype/backend/hospiproject/src/main/java/mk/ukim/finki/hospiproject/HospiprojectApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.hospiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospiprojectApplication.class, args);
+	}
+
+}
