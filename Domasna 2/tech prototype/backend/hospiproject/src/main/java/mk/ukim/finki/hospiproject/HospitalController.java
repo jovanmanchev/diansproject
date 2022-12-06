69a08b4633d221d3746a171877c82a6658c1bdb1
Pelivanov Mile <milepelivanov02@gmail.com>
2022-12-06 12:14:55 +0100

    Creating a FrontEnd Directory

--- Domasna 2/tech prototype/backend/hospiproject/src/main/java/mk/ukim/finki/hospiproject/HospitalController.java
+++ Domasna 2/tech prototype/backend/hospiproject/src/main/java/mk/ukim/finki/hospiproject/HospitalController.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.hospiproject;
 import mk.ukim.finki.hospiproject.repository.dataholder.DataReader;
 import mk.ukim.finki.hospiproject.repository.dataholder.Hospital;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 @RestController
 public class HospitalController {
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/hospitals")
     public ResponseEntity<List<Hospital>> getHospitals(){
         return ResponseEntity.ok()
