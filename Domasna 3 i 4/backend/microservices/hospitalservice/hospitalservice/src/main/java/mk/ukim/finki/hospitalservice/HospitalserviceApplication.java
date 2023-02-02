ff94a000db6116eb9e23340175b71a35b091a6f9
jovanmanchev <jovanmanchev3003@gmail.com>
2023-02-02 13:19:29 +0100

    hospitals microservice added

---
+++ Domasna 3 i 4/backend/microservices/hospitalservice/hospitalservice/src/main/java/mk/ukim/finki/hospitalservice/HospitalserviceApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.hospitalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalserviceApplication.class, args);
+	}
+
+}
---
+++ Domasna 3 i 4/backend/microservices/hospitalservice/hospitalservice/src/main/java/mk/ukim/finki/hospitalservice/repository/dataholder/DataReader.java
@@ -0,0 +1,43 @@
+package mk.ukim.finki.hospitalservice.repository.dataholder;
+
+import org.json.simple.JSONObject;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DataReader {
+    private final String FILE_PATH = "./hospitals.txt";
+    public static List<JSONObject> jsonObjects = new ArrayList<>();
+    private File file;
+
+    public static List<Hospital> hospitalList;
+    public DataReader(){
+        file = new File(FILE_PATH);
+        hospitalList = new ArrayList<>();
+    }
+    public void extractDataFromFile() throws FileNotFoundException, IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        hospitalList = bufferedReader.lines()
+                .map(Hospital::createHospital)
+                .collect(Collectors.toList());
+
+
+    }
+
+    public void generateJSONData() throws IOException {
+
+
+        jsonObjects = this.hospitalList.stream().map(hospital -> {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", hospital.getHospitalId());
+            jsonObject.put("lon", hospital.getLon());
+            jsonObject.put("lat", hospital.getLat());
+            jsonObject.put("name", hospital.getName());
+            return jsonObject;
+        }).collect(Collectors.toList());
+
+
+    }
+}
